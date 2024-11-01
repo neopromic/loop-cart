3edb951f4df4d333029300e52479d08e00e4c02f
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-31 21:55:07 -0300

    first commit

---
+++ src/app/[locale]/(unauth)/page.tsx
@@ -0,0 +1,29 @@
+import { useTranslations } from 'next-intl';
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+
+export async function generateMetadata(props: { params: { locale: string } }) {
+  const t = await getTranslations({
+    locale: props.params.locale,
+    namespace: 'Index',
+  });
+
+  return {
+    title: t('meta_title'),
+    description: t('meta_description'),
+  };
+}
+
+const Index = (props: { params: { locale: string } }) => {
+  unstable_setRequestLocale(props.params.locale);
+  const t = useTranslations('Index');
+
+  return (
+    <>
+      <h1>{t('meta_title')}</h1>
+      <p>{t('meta_description')}</p>
+
+    </>
+  );
+};
+
+export default Index;
---
+++ src/components/DemoBadge.tsx
@@ -0,0 +1,8 @@
+export const DemoBadge = () => (
+  <div className="fixed bottom-0 right-20 z-10">
+    <div className="rounded-md bg-gray-900 px-3 py-2 font-semibold text-gray-100">
+      <span className="text-gray-500">Criado por</span>
+      {' Infinity Team'}
+    </div>
+  </div>
+);
