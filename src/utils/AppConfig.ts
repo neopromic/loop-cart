221fa086bf94520338cf04da35e1f8cd4fab423c
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-31 23:36:08 -0300

    feat: change the default locale to pt-br

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -5,7 +5,7 @@ const localePrefix: LocalePrefix = 'as-needed';
 // FIXME: Update this configuration file based on your project information
 export const AppConfig = {
   name: 'LoopCart',
-  locales: ['en', 'fr'],
-  defaultLocale: 'en',
+  locales: ['en', 'fr', 'pt-br'],
+  defaultLocale: 'pt-br',
   localePrefix,
 };
