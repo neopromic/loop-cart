1a99a3e46db340c2344f2f9beafd0f61d06ecb01
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-11-01 09:51:45 -0300

    feat: add product card and hero

--- src/app/[locale]/(unauth)/page.tsx
+++ src/app/[locale]/(unauth)/page.tsx
@@ -1,5 +1,7 @@
-import { useTranslations } from 'next-intl';
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+
+import { ProductCard } from '@/components/CardProduct';
+import { Hero } from '@/layouts/Home';
 
 export async function generateMetadata(props: { params: { locale: string } }) {
   const t = await getTranslations({
@@ -12,16 +14,20 @@ export async function generateMetadata(props: { params: { locale: string } }) {
     description: t('meta_description'),
   };
 }
-
 const Index = (props: { params: { locale: string } }) => {
   unstable_setRequestLocale(props.params.locale);
-  const t = useTranslations('Index');
+  // const t = useTranslations('Index');
 
   return (
     <>
-      <div>
-        <h1>{t('hero_title')}</h1>
-      </div>
+      <Hero />
+      <section className="space-y-4 p-6 lg:px-12">
+        <h1 className="font-light tracking-tight">Mais vendidos</h1>
+        <div className="">
+
+          <ProductCard productImage="https://github.com/neopromic.png" productName="Test" productPrice={70} />
+        </div>
+      </section>
     </>
   );
 };
