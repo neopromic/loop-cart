8a5b75711a3160f259228bc4bfa3dc09684bf76a
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-11-01 23:52:38 -0300

    feat: :sparkles: improve the card product component

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -1,6 +1,7 @@
+import { Check } from 'lucide-react';
 import Image from 'next/image';
 
-import { Button } from './ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
 
 type ProductCardProps = {
   productName: string;
@@ -8,20 +9,35 @@ type ProductCardProps = {
   productImage: string;
 };
 
-export const ProductCard = ({
-  productImage,
-  productName,
-  productPrice,
-}: ProductCardProps) => {
+export const ProductCard = ({ productImage, productName, productPrice }: ProductCardProps) => {
+  const formattedPrice = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(productPrice);
+
   return (
-    <div className="productCardContainer">
-      <Image src={productImage} alt="Product image" width={1000} height={1000} className="" />
-      <h2>{productName}</h2>
-      <p>
-        R$
-        {productPrice}
-      </p>
-      <Button>Add to Cart</Button>
-    </div>
+    <Card className="w-full basis-1/2 rounded-lg lg:w-96">
+      <CardHeader className="p-0">
+        <Image
+          src={productImage}
+          alt={productName}
+          width={1000}
+          height={1000}
+          className="h-64 w-full rounded-lg object-cover"
+        />
+      </CardHeader>
+      <CardContent className="pt-6">
+        <CardTitle className=" truncate text-wrap text-base font-normal">
+          {productName}
+        </CardTitle>
+        <CardDescription aria-label={`Preço: ${formattedPrice}`} className="text-xl font-medium text-foreground">
+          {formattedPrice}
+        </CardDescription>
+        <p className="flex items-center gap-2 text-sm text-green-900">
+          <Check />
+          Entrega automática
+        </p>
+      </CardContent>
+    </Card>
   );
 };
